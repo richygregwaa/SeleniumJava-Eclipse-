b41e7dcd9a20e1de722145b59c51bde999e500d5
richy <richygregwa@hotmail.com>
2020-09-25 23:27:20 +0100

    Modify ListenerDemoTest
    1. First delete this class (ListenerDemoTest) from 'Git Repositories' in
    eclipse (Note - ignore what appears in 'Unstaged Changes' showing
    deletion as this will correct itself.
    2. Export this class from Package Explorer in eclipse
    3. Refresh in Git Repository in eclipse
    4. right-click this class in git repository and add to index.
    5. Now 'Unstaged Changes' will clear and 'Staged Changes' will now show
    an asterist for amendment
    6. Click Commit and Push - you know you want to

--- webportal/src/test/java/ui/ListenerDemoTest.java
+++ webportal/src/test/java/ui/ListenerDemoTest.java
@@ -1,5 +1,6 @@
 // How to capture screenshots for failed tests in TestNG
 // Following classes are used together: ListenerDemoTest, BaseTest, testUtils, Listeners, testng4.xml
+// Following classes are used together: ListenerDemoTest, Retry, RetryListener, testng5.xml
 
 package ui;
 
@@ -8,14 +9,15 @@ import org.testng.annotations.Test;
 
 import common.BaseTest;
 
-public class ListenerDemoTest extends BaseTest{
-	
+public class ListenerDemoTest extends BaseTest {
+
+//	@Test(retryAnalyzer = common.Retry.class)
+//  ( * * The above '@Test' line is now set as a Listener in testng5.xml therefore parameters in brackets have been removed* *)	
 	@Test
-	public void launchApp()
-	{
+
+	public void launchApp() {
 		driver.get("https://ebay.com");
 		Assert.assertTrue(false);
 	}
-	
 
 }
