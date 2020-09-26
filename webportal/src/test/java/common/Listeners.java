e719798060e518177484738b3dfeceaa34a94a5e
richy <richygregwa@hotmail.com>
2020-09-26 19:13:07 +0100

    How to write a log to reports that can be seen in the test-output folder. Following classes are used together: Listeners, ReportsTest, testng6
    By adding the Listener 'Listeners' to testng it was possible to have
    reports for all methods in the ReportsTest class

--- webportal/src/test/java/common/Listeners.java
+++ webportal/src/test/java/common/Listeners.java
@@ -1,4 +1,5 @@
 //  Following classes are used together: ListenerDemoTest, BaseTest, testUtils, Listeners, testng4.xml
+// Following classes are used together: Listeners, ReportsTest, testng6
 
 package common;
 
@@ -7,16 +8,19 @@ import java.io.IOException;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 
 import utilities.testUtils;
 
 public class Listeners extends testUtils implements ITestListener{
 	
 	public void onTestStart(ITestResult result) {
+		Reporter.log("Method name is - " + result.getName());
 		System.out.println("Test case is starting");
 	}
 
 	public void onTestSuccess(ITestResult result) {
+		Reporter.log("Status of execution is - " + result.getStatus());
 		
 	}
 
