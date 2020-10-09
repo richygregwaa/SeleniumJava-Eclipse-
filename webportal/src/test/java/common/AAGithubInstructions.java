25ae2c292a0d2c5a6fa7f81d4e2a5cdd71a6b77a
richy <richygregwa@hotmail.com>
2020-10-09 19:22:47 +0100

    Instructions for GITHUB

---
+++ webportal/src/test/java/common/AAGithubInstructions.java
@@ -0,0 +1,25 @@
+// Delete
+// If deleting files, delete them first from the Working Tree in Git Repositories window. 
+// This window can be opened from the Window > Show View > Other > Git > Git Repositories
+// To delete just righ-click the java/xml file and select Delete.
+// In the Git Repositories window right-click SelenuimJava-Eclipse and select commit
+// In the Git Repositories window right-click SelenuimJava-Eclipse and select 'Add to staging' - 
+//   only the files you have deleted should appear in the Staged changes.
+// Select Commit and Push
+
+// Amend
+// If amending files then you will need to delete them from the Git Repository first as shown above.
+// Now you will need to add the same files from Project Explorer (updated files) back into Git Repositories window - 
+// To do this follow the Add instructions below
+
+// Add
+// In the Project explorer highlight the new files you have added or updated(if updated see 'Amend' above first) - 
+//   (you can hold down the Control button to select several)
+// Once files are selected right-click and select Export
+// In Git Repositories window click on the button with 2 swirling arrows at the top to refresh 
+// In Git Repositories window the files that were deleted should now reappear. These will be the new/updated files
+// In the Git Repositories window right-click SelenuimJava-Eclipse and select Commit
+// In the Git Repositories window right-click Working Tree and select 'Add to index' - 
+//  Only the newly added files will appear in the  Staged Changes
+// Add a Commit Message
+// Select Commit and Push
